535c136573899c1366e44891a6e3cbb545ba43c8
Karen Baque <k.baque@aol.com>
2017-09-10 23:13:19 -0400

    setup

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+$('h2').click(function(){
+    alert("The paragraph was clicked.");
+});
+
+// let myFunction= function(){
+//     alert("bob")
+//     document.getElementById("ex1").innerHTML = "Paragraph changed!";
+// }
